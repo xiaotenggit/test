339cf4adc44e8aeac08798d41d9fe05853952266
xiaotenggit <130160682+xiaotenggit@users.noreply.github.com>
2023-04-07 23:40:12 +0800

    上传变量文件
    第一次使用Github上传文件，操作一把

---
+++ 02_变量.py
@@ -0,0 +1,13 @@
+qq_num = "123456"
+qq_password = "abc123"
+
+print(qq_num)
+print(qq_password)
+
+
+apple_price = 7.5
+apple_weight = 8.5
+money = apple_price * apple_weight
+money = money - 5  # 只要买苹果就减5块钱
+
+print(money)
